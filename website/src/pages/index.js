d68100b7ec7498c489ec3d72e610465a75c08006
AngeloCG97 <angelocas13_8@hotmail.com>
2020-07-23 17:06:36 -0600

    update medium section

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -113,7 +113,6 @@ const DarkTheme = makeStyles({
 });
 
 const HeroSection  = () => {
-
   const images = ["https://raw.githubusercontent.com/eoscostarica/guide.eoscostarica.io/master/website/static/img/cards-icons/heroe.png"];
   const background = images[Math.floor(Math.random() * images.length)];
   const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
@@ -143,10 +142,8 @@ const HeroSection  = () => {
   );
 }
 
-const CustomCard= (props) => {
-
+const CustomCard = (props) => {
   const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
-  
   let classes=null;
 
   if(!isDarkTheme){
@@ -231,26 +228,73 @@ const CardsSection = () => {
   );
 }
 
+const CustomArticle= (props) =>{
+  const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
+  let classes=null;
+  if(!isDarkTheme){
+     classes = lightTheme();
+  }
+  else{
+     classes = DarkTheme();
+  }
+
+  return (
+    <Card className="cardroot" className={classes.card} variant="outlined">
+      <CardMedia
+          className={classes.media}
+          image={props.img}
+        />
+      <CardContent>
+        <Typography className={classes.overline} gutterBottom>ARTICLES</Typography>
+        <Typography className={classes.title}  gutterBottom variant="h5" component="h2">{props.title}</Typography>
+        <Typography className={classes.body} variant="body2" component="p">{props.body}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button className={classes.button} href={props.href} >Read More</Button>    
+      </CardActions>
+  </Card>
+  );
+}
+
 const MediumSection = () => {
 
-  try {
-    var widget = document.getElementById("medium-widget");
-    if(widget){
-      window.mediumWidget();
-    }
-  }catch(e){
-    window.location.reload();
-  }
   return (
     <div className="container section-container">
-      <h1 className="section-tittle">Artículos de Medium</h1>
-      <div id="medium-widget"></div>
+       <h1 className="section-tittle">Read our Medium articles</h1>
+       <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={2}
+      >
+      <Grid item xs={12} sm={12} md={4}>
+        <CustomArticle 
+          title="Get to Know EOS Costa Rica"
+          body= "Learn more about our history, team and projects we carry out."
+          href= "https://medium.com/@eoscostarica/get-to-know-eos-costa-rica-f91f5b0bb7c4"
+          img="https://miro.medium.com/max/700/1*x7OW2wAhH1YapNVZl_-SEA.png"></CustomArticle>
+      </Grid>
+      <Grid item xs={12} sm={12} md={4}>
+        <CustomArticle 
+          title="You Should Participate in an Open-Source Project"
+          body= "We explain what the EOSIO blockchain protocol consists of."
+          href= "https://medium.com/@eoscostarica/you-should-participate-in-an-open-source-project-c259f8759d41"
+          img="https://miro.medium.com/max/875/1*_eTme4t7HJfE7aeeyd3kxQ.jpeg"></CustomArticle>
+      </Grid>
+      <Grid item xs={12} sm={12} md={4}>
+        <CustomArticle 
+          title="What Is Blockchain?"
+          body= "We explain, with examples, what a blockchain is and how it works."
+          href= "https://medium.com/@eoscostarica/what-is-blockchain-an-introduction-9535ed3e6005"
+          img="https://miro.medium.com/max/875/1*ohmD1qq4dAUOu4G-gXtaqA.jpeg"></CustomArticle>
+      </Grid>
+    </Grid>
     </div>
   );
 }
 
 const HomePage = () => {
-
   const {siteConfig} = useDocusaurusContext();
 
   return (
