ec6cd77b2dcee5d93ba07be922f58d9c0242ecad
Xavier Fernandez <xavier@crservers.com>
2020-08-03 16:50:55 -0600

    .gitignore fix

--- website/.docusaurus/client-modules.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/rodrigofernandez/dev/guide.eoscostarica.io/website/src/css/custom.css"),
-];
