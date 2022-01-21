fb93b6216416cf250950ee3a49f3e8c71ef43aa8
Paola Espinoza <71234240+hanakoori01@users.noreply.github.com>
2022-01-21 15:21:19 -0600

    feat/incorporate new changes oss projects (#419)
    * feat(docs): add local environments section
    
    * (docs): code review change local development environments
    
    * (docs): second update to oss projects
    
    * (docs): fix css problems
    
    * feat(docs): add  metadata
    
    * (docs): add metadata to layout
    
    * (docs): last update
    
    * (docs): change metadata
    
    * (docs): add metaImgBlack
    
    * (docs): remove unused variables
    
    Co-authored-by: Paola Espinoza <paolamariam2001@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import useThemeContext from '@theme/hooks/useThemeContext';
 import { Grid } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Card } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 import '../css/homepage.css';
+
+const MetaData={
+  title:"EOS Costa Rica | Developers Guides",
+  description:"Documentation for EOS Costa Rica developers.",
+  img:"../static/img/metaImgBlack.png",
+  hrefLangPath: "https://guide.eoscostarica.io/"
+}
 
 const lightTheme = makeStyles({
 
@@ -305,16 +308,16 @@ const MediumSection = () => {
 }
 
 const HomePage = () => {
-  const {siteConfig} = useDocusaurusContext();
 
   return (
     <Layout
-      permalink="/"
-      title={siteConfig.title}
-      description={siteConfig.tagline}>
-      <HeroSection></HeroSection>
-      <CardsSection></CardsSection>
-      <MediumSection></MediumSection>
+      title={MetaData.title}
+      description={MetaData.description}
+      image={MetaData.img}
+      hrefLangPath={MetaData.hrefLangPath}>
+      <HeroSection/>
+      <CardsSection/>
+      <MediumSection/>
     </Layout>
   );
   
