a0a948bfd8259695889508e006091fbb3e771699
Paola Espinoza <paolamariam2001@gmail.com>
2022-02-14 16:53:58 -0600

    (docs): change meta image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,23 +9,24 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'EOS Costa Rica | Developers Guides',
-  tagline: 'Documentation for developers of EOS Costa Rica',
-  url: 'https://guide.eoscostarica.io',
-  baseUrl: '/', // Base URL for your project */
+  title: "EOS Costa Rica | Developers Guides",
+  tagline: "Documentation for developers of EOS Costa Rica",
+  url: "https://guide.eoscostarica.io",
+  baseUrl: "/", // Base URL for your project */
   //cname: "guide.eoscostarica.io",
   // Used for publishing and more
-  projectName: 'guide.eoscostarica.io',
-  organizationName: 'eoscostarica',
-  favicon: 'img/favicon/favicon.ico',
+  projectName: "guide.eoscostarica.io",
+  organizationName: "eoscostarica",
+  favicon: "img/favicon/favicon.ico",
   //scripts: ['https://buttons.github.io/buttons.js','../../scripts/languageSelector.js'],
   themeConfig: {
-    image: 'img/metaImgBlack.png',
+    image: "img/metaImg.jpg",
     navbar: {
       logo: {
-        alt: 'EOS Costa Rica Logo',
-        src: 'https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent.png',
-        srcDark: 'https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent-white.png'
+        alt: "EOS Costa Rica Logo",
+        src: "https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent.png",
+        srcDark:
+          "https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent-white.png",
       },
       items: [
         /*{
@@ -33,75 +34,91 @@ const siteConfig = {
           label: 'Inicio',
           position: 'left',
         },*/
-          {to: 'docs/open-source-guidelines', label: 'Developer Guidelines', position: 'left'},
-          {to: 'docs/eos-learn/eosio-protocol', label: 'Learn EOSIO', position: 'left'},
-          {to: 'docs/tutorials/markdown-guide', label: 'Tutorials', position: 'left'},
-          {to: 'docs/tools/glossary', label: 'Tools', position: 'left'},
-          {to: 'docs/community-resources/eosio-networks', label: 'Community', position: 'left'},
-          {to: 'https://eoscostarica.io/', label: 'Website', position: 'left'},
-          {
-            href: 'https://guias.eoscostarica.io/',
-            label: 'Español',
-            position: 'right',
-          },
-          {to: 'search'},
+        {
+          to: "docs/open-source-guidelines",
+          label: "Developer Guidelines",
+          position: "left",
+        },
+        {
+          to: "docs/eos-learn/eosio-protocol",
+          label: "Learn EOSIO",
+          position: "left",
+        },
+        {
+          to: "docs/tutorials/markdown-guide",
+          label: "Tutorials",
+          position: "left",
+        },
+        { to: "docs/tools/glossary", label: "Tools", position: "left" },
+        {
+          to: "docs/community-resources/eosio-networks",
+          label: "Community",
+          position: "left",
+        },
+        { to: "https://eoscostarica.io/", label: "Website", position: "left" },
+        {
+          href: "https://guias.eoscostarica.io/",
+          label: "Español",
+          position: "right",
+        },
+        { to: "search" },
       ],
     },
     footer: {
       logo: {
-        alt: 'EOS Costa Rica Logo',
-        src: 'https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent.png',
-        href: 'https://guide.eoscostarica.io/',
+        alt: "EOS Costa Rica Logo",
+        src: "https://raw.githubusercontent.com/eoscostarica/design-assets/master/logos/eosCR/byw-horizontal-transparent.png",
+        href: "https://guide.eoscostarica.io/",
       },
       links: [
         {
-          title: 'EOS Costa Rica',
+          title: "EOS Costa Rica",
           items: [
             {
-              label: 'Engineering Culture',
-              to: '/docs/engineering-culture',
+              label: "Engineering Culture",
+              to: "/docs/engineering-culture",
             },
             {
-              label: 'Open Source Projects',
-              to: '/docs/open-source-projects',
+              label: "Open Source Projects",
+              to: "/docs/open-source-projects",
             },
             {
-              label: 'Open Source Guidelines',
-              to: '/docs/open-source-guidelines',
+              label: "Open Source Guidelines",
+              to: "/docs/open-source-guidelines",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Meetup',
-              href: 'https://www.meetup.com/es/EOS-Costa-Rica/',
+              label: "Meetup",
+              href: "https://www.meetup.com/es/EOS-Costa-Rica/",
             },
             {
-              label: 'Telegram channels',
-              href: 'https://t.me/eoscr',
+              label: "Telegram channels",
+              href: "https://t.me/eoscr",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/eoscostarica/',
+              label: "GitHub",
+              href: "https://github.com/eoscostarica/",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/company/eoscostarica/',
+              label: "Linkedin",
+              href: "https://www.linkedin.com/company/eoscostarica/",
             },
             {
-              label: 'Medium',
-              href: 'https://medium.com/@eoscostarica',
+              label: "Medium",
+              href: "https://medium.com/@eoscostarica",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/eoscostarica',
+              label: "Twitter",
+              href: "https://twitter.com/eoscostarica",
             },
           ],
         },
@@ -110,31 +127,31 @@ const siteConfig = {
     },
     //Algolia integration
     algolia: {
-      apiKey: 'b385fd20b79dbd8086bb917c038de56e',
-      indexName: 'eoscostarica',
-      placeholder: 'Search',
-      algoliaOptions: {} // Optional, if provided by Algolia
+      apiKey: "b385fd20b79dbd8086bb917c038de56e",
+      indexName: "eoscostarica",
+      placeholder: "Search",
+      algoliaOptions: {}, // Optional, if provided by Algolia
     },
     googleAnalytics: {
       trackingID: "UA-173987-66",
     },
-  }, 
+  },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           // Docs folder path relative to website dir.
-          path: './docs',
+          path: "./docs",
           // Sidebars file relative to website dir.
           editUrl:
-            'https://github.com/eoscostarica/guide.eoscostarica.io/tree/master/',
-          sidebarPath: require.resolve('./sidebars.js'),
+            "https://github.com/eoscostarica/guide.eoscostarica.io/tree/master/",
+          sidebarPath: require.resolve("./sidebars.js"),
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
@@ -142,16 +159,14 @@ const siteConfig = {
 
   scripts: [
     {
-      src:
-        'https://medium-widget.pixelpoint.io/widget.js',
+      src: "https://medium-widget.pixelpoint.io/widget.js",
       async: true,
     },
     {
-      src:
-        '../../static/scripts/mediumScript.js',
+      src: "../../static/scripts/mediumScript.js",
       async: true,
     },
-    "https://platform.twitter.com/widgets.js"
-  ]
+    "https://platform.twitter.com/widgets.js",
+  ],
 };
 module.exports = siteConfig;
