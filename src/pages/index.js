b875d9862318bcda19f0deb935fe56be85235f86
JAMares <josuegerardojgam885@gmail.com>
2022-07-21 13:14:53 -0600

    feat(frontend): Requested changes fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 import { StartResourceSection } from "./startResource/index";
 import { HeroSection } from "./heroSection/index";
-import { MediumSection } from "./BlogCard";
+import { MediumSection } from "./CustomArticle";
 
 export const HomePage = () => {
   const { siteConfig } = useDocusaurusContext();
@@ -13,7 +13,8 @@ export const HomePage = () => {
     <Layout
       permalink="/"
       title={siteConfig.title}
-      description={siteConfig.tagline}>
+      description={siteConfig.tagline}
+    >
       <HeroSection />
       <StartResourceSection />
       <MediumSection />
